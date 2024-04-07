f298ccf84f952e113258fa649e8c6145b8e2a84c
iu363bTikhonova <viktoria.crouch@yandex.ru>
2024-04-07 15:05:54 +0300

    lab4

--- fclient/app/build.gradle.kts
+++ fclient/app/build.gradle.kts
@@ -65,7 +65,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("commons-codec:commons-codec:1.16.1")
-
+    implementation ("org.apache.commons:commons-io:1.3.2")
 
 
 
--- fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
+++ fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -11,10 +11,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import android.util.Log;
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.io.IOUtils;
 
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class MainActivity extends AppCompatActivity implements TransactionEvents{
 
     // Used to load the 'native-lib' library on application startup.
@@ -68,8 +74,9 @@ public class MainActivity extends AppCompatActivity implements TransactionEvents
             }
         }).start();
          */
-        byte[] trd = stringToHex("9F0206000000000100");
-        transaction(trd);
+        //byte[] trd = stringToHex("9F0206000000000100");
+        //transaction(trd);
+        testHttpClient();
     }
     private String pin;
 
@@ -121,7 +128,38 @@ public class MainActivity extends AppCompatActivity implements TransactionEvents
         return hex;
     }
 
+    protected void testHttpClient()
+    {
+        new Thread(() -> {
+            try {
+                HttpURLConnection uc = (HttpURLConnection) (new URL("http://10.0.2.2:8081/api/v1/title").openConnection());
+                InputStream inputStream = uc.getInputStream();
+                String html = IOUtils.toString(inputStream);
+                String title = getPageTitle(html);
+                runOnUiThread(() ->
+                {
+                    Toast.makeText(this, title, Toast.LENGTH_LONG).show();
+                });
 
+            } catch (Exception ex) {
+                Log.e("fapptag", "Http client fails", ex);
+            }
+        }).start();
+    }
+
+
+
+    protected String getPageTitle(String html)
+    {
+        Pattern pattern = Pattern.compile("<title>(.+?)</title>", Pattern.DOTALL);
+        Matcher matcher = pattern.matcher(html);
+        String p;
+        if (matcher.find())
+            p = matcher.group(1);
+        else
+            p = "Not found";
+        return p;
+    }
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
